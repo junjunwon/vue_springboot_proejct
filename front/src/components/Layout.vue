53a5fe33196fcaa6520103a22d3003e5f75d6aa8
junjunwon <wnsgh1993@gmail.com>
2021-09-30 10:27:50 +0900

    front file commit

---
+++ front/src/components/Layout.vue
@@ -0,0 +1,16 @@
+<template>
+    <div id="pagewrap">
+        <header>
+            <slot name="header"></slot>
+        </header>
+        <nav>
+            <slot name="nav"></slot>
+        </nav>
+        <section id="content">
+            <slot name="content"></slot>
+        </section>
+        <footer>
+            <slot name="footer"></slot>
+        </footer>
+    </div>
+</template>
---
+++ front/vue.config.js
@@ -0,0 +1,20 @@
+const path = require('path');
+
+module.exports = {
+    outputDir : "../src/main/resources/static",
+    devServer : {
+        port : 3030,
+        proxy : {
+            '/' : {
+                target : 'http://localhost:3000'
+            }
+        }
+    },
+    configureWebpack:{
+        resolve : {
+            alias : {
+                'BOARD' : path.join(__dirname, 'src/components/board') //자주쓰는 경로를 BOARD로 묶어 가독성을 높인다.
+            }
+        }
+    }
+}
